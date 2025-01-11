337e2c7ac55389f7e2f6af920267b3e4803c15e2
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2025-01-12 02:00:53 +0530

    views, text, scrollview, image, buttons, styling

---
+++ App.jsx
@@ -0,0 +1,90 @@
+import { 
+  StyleSheet, 
+  Text, 
+  // SafeAreaView, 
+  ScrollView,
+  View,
+  Image, 
+  Pressable, 
+  // Button, 
+  // TouchableOpacity, 
+  useColorScheme 
+} from 'react-native'
+import React from 'react'
+
+const App = () => {
+  let theme = useColorScheme();
+  let bgColor = (theme === 'dark') ? '#000' : '#fff';
+  let textColor = (theme === 'dark') ? '#fff' : '#000';
+  let boxNum = [1, 2, 3, 4, 5];
+
+  return (
+    <ScrollView 
+      contentContainerStyle={[styles.container, {backgroundColor: bgColor, alignItems: 'center', gap: 20}]} 
+      horizontal={false}
+    >
+      <View>
+        <Text style={[styles.text, {color: textColor}]}>Hello, I'm Kanishk Rastogi and I'm learning React Native!!</Text>
+      </View>
+      
+      <Image 
+        style={styles.image}
+        source={{uri:"https://avatars.githubusercontent.com/u/113916868?v=4"}} 
+      />
+
+      {/* 
+      <Button title='Button' onPress={()=>alert("Button hu mai!")} />
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() =>  alert('Touchable hu mai!')}>
+        <Text style={styles.button}>TouchableOpacity</Text>
+      </TouchableOpacity> 
+      */}
+
+      <View>
+        <Pressable 
+          style={styles.button}
+          onPress={() => alert('Pressable hu mai!')}>
+          <Text style={styles.text}>Alert</Text>
+        </Pressable>
+      </View>
+      
+      {
+        boxNum.map((num) => {
+          return <View key={num} style={[styles.box, {backgroundColor: (num%2 === 0) ? 'green' : 'red'}]} />
+        })
+      }
+    </ScrollView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 20,
+    },
+    image:{
+        width: "100%",
+        height: "50%",
+    },
+    text:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    button:{
+        backgroundColor: '#f0f0f0',
+        padding: 20,
+        borderRadius: 5,
+        width: "40%",
+        alignSelf: 'center',
+    },
+    box:{
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+    }
+});
